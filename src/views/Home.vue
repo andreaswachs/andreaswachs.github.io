67bcb53c68aa70d8f613efcfcdb0c58d6101e111
Andreas Wachs Hjalager <ahja@itu.dk>
2021-05-06 22:30:24 +0200

    Remove about-flavoured text from home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,7 @@
 <template>
   <content-row>
     <h2>Hello, World!</h2>
-    <p class="flow-text">Welcome to my personal website</p>
-
-    <p>
-      I'm a software development student, with a heart and soul that belongs mainly in back-end development.
-      Since starting my studies, I have become increasingly interested in functional programming. I find the
-      discrete mathematics field very interesting &mdash; although my performance at the first semester exam
-      in discrete mathematics were just passing grades.
-    </p>
-
+    
     <p>
       I've created this site to publish posts on various programming/CS topics, where I find something interesting to share.
       This site is a personal product after taking my 2nd semester course 
