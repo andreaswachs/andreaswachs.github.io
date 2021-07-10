3c4855d764a313cb5652178e0adc4489332d5c8b
Andreas Wachs Hjalager <ahja@itu.dk>
2021-07-10 18:16:14 +0200

    Applied formatting and tightened up visuals

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,22 +1,31 @@
 <template>
-  <div class="about">
+  <content-row>
+    <div class="about">
+      <h2>Who am I?</h2>
 
-    <h2>Who am I?</h2>
+      <div class="row valign-wrapper">
+        <div class="col s0 m2 hide-on-med-and-down show-on-medium">
+          <img src="images/headshot.png" class="responsive-img circle" />
+        </div>
 
-    <div class="row valign-wrapper">
-      <div class="col s0 m2 hide-on-med-and-down show-on-medium">
-        <img src="images/headshot.png" class="responsive-img circle" />
-      </div>
-      
-      <div class="col s12 m10">
-        <p>
-          I'm a software development student, with a heart and soul that belongs mainly in back-end development.
-          Since starting my studies, I have become increasingly interested in functional programming. I find the
-          discrete mathematics field very interesting &mdash; although certainly not a naturally gifted mathematics student.
-        </p>
+        <div class="col s12 m10">
+          <p>
+            I'm a software development student, with a heart and soul that
+            belongs mainly in back-end development. Since starting my studies, I
+            have become increasingly interested in functional programming. I
+            find the discrete mathematics field very interesting &mdash;
+            although certainly not a naturally gifted mathematics student.
+          </p>
+        </div>
       </div>
     </div>
-
-
-  </div>
+  </content-row>
 </template>
+<script>
+import ContentRow from "../components/ContentRow.vue";
+
+export default {
+  components: { ContentRow },
+};
+</script>
+
